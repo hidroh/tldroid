ad5f9ccd07d2e2ce51f98af0d44cd75bcaf1e4c6
Ha Duy Trung <haduytrung@gmail.com>
2015-12-29 22:49:31 +0800

    Use data binding for dropdown items

--- app/src/main/java/io/github/hidroh/tldroid/Bindings.java
+++ app/src/main/java/io/github/hidroh/tldroid/Bindings.java
@@ -1,5 +1,8 @@
 package io.github.hidroh.tldroid;
 
+import android.database.Cursor;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
 import android.widget.TextView;
 
@@ -9,6 +12,40 @@ public class Bindings {
     public static void setFont(TextView textView, boolean enabled) {
         if (enabled) {
             textView.setTypeface(Application.MONOSPACE_TYPEFACE);
+        }
+    }
+
+    public static class Command extends BaseObservable {
+        private String name;
+        private String platform;
+
+        public static Command fromProvider(Cursor cursor) {
+            Command command = new Command();
+            command.name = cursor.getString(cursor.getColumnIndexOrThrow(
+                    TldrProvider.CommandEntry.COLUMN_NAME));
+            command.platform = cursor.getString(cursor.getColumnIndexOrThrow(
+                    TldrProvider.CommandEntry.COLUMN_PLATFORM));
+            return command;
+        }
+
+        @Bindable
+        public String getName() {
+            return name;
+        }
+
+        @Bindable
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Bindable
+        public String getPlatform() {
+            return platform;
+        }
+
+        @Bindable
+        public void setPlatform(String platform) {
+            this.platform = platform;
         }
     }
 }

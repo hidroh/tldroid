69e419d9157691ab6dae9d7fad8dafa47cf011b0
Ha Duy Trung <haduytrung@gmail.com>
2016-01-29 23:34:40 +0800

    Allow running commands

--- app/src/main/java/io/github/hidroh/tldroid/CommandActivity.java
+++ app/src/main/java/io/github/hidroh/tldroid/CommandActivity.java
@@ -24,15 +24,17 @@ public class CommandActivity extends AppCompatActivity {
     public static final String EXTRA_QUERY = CommandActivity.class.getName() + ".EXTRA_QUERY";
     public static final String EXTRA_PLATFORM = CommandActivity.class.getName() + ".EXTRA_PLATFORM";
     private static final String STATE_CONTENT = "state:content";
+    private static final String PLATFORM_OSX = "osx";
     private String mContent;
     private String mQuery;
+    private String mPlatform;
     private ViewDataBinding mBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mQuery = getIntent().getStringExtra(EXTRA_QUERY);
-        String platform = getIntent().getStringExtra(EXTRA_PLATFORM);
+        mPlatform = getIntent().getStringExtra(EXTRA_PLATFORM);
         setTitle(mQuery);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_command);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
@@ -54,8 +56,8 @@ public class CommandActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             mContent = savedInstanceState.getString(STATE_CONTENT);
         }
-        if (mContent == null && !TextUtils.isEmpty(platform)) {
-            new GetCommandTask(this, platform).execute(mQuery);
+        if (mContent == null && !TextUtils.isEmpty(mPlatform)) {
+            new GetCommandTask(this, mPlatform).execute(mQuery);
         } else {
             render(mContent);
         }
@@ -63,6 +65,7 @@ public class CommandActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_run, menu);
         getMenuInflater().inflate(R.menu.menu_share, menu);
         return super.onCreateOptionsMenu(menu);
     }
@@ -79,6 +82,8 @@ public class CommandActivity extends AppCompatActivity {
                             .putExtra(Intent.EXTRA_SUBJECT, mQuery)
                             .putExtra(Intent.EXTRA_TEXT, Html.fromHtml(mContent).toString()));
         }
+        menu.findItem(R.id.menu_run).setVisible(visible &&
+                !PLATFORM_OSX.equalsIgnoreCase(mPlatform));
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -86,6 +91,11 @@ public class CommandActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
+            return true;
+        }
+        if (item.getItemId() == R.id.menu_run) {
+            startActivity(new Intent(this, RunActivity.class)
+                .putExtra(RunActivity.EXTRA_COMMAND, mQuery));
             return true;
         }
         return super.onOptionsItemSelected(item);

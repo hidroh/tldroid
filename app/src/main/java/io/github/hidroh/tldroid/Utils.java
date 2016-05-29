98bcc21ebd20241d3aa7eb546410993dcbb970c6
Ha Duy Trung <haduytrung@gmail.com>
2016-05-29 22:49:50 +0800

    Use ConstraintLayout

--- app/src/main/java/io/github/hidroh/tldroid/Utils.java
+++ app/src/main/java/io/github/hidroh/tldroid/Utils.java
@@ -1,27 +1,12 @@
 package io.github.hidroh.tldroid;
 
-import android.os.Build;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import okio.Okio;
 
-public class Utils {
-    public static String readUtf8(InputStream inputStream) throws IOException {
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.FROYO) {
-            // Okio uses some Java String APIs that are only available in later Android versions
-            return Okio.buffer(Okio.source(inputStream)).readUtf8();
-        }
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        br.close();
-        return sb.toString();
+class Utils {
+    static String readUtf8(InputStream inputStream) throws IOException {
+        return Okio.buffer(Okio.source(inputStream)).readUtf8();
     }
 }
